ddc97bdb4a3f3a06d1bd8ba13d52b4e34e81685e
joaquinvesapa <joaquinvesapa@gmail.com>
2023-10-21 00:24:24 -0300

    mover imagen y guardar la ruta

--- App/Helpers/functions.php
+++ App/Helpers/functions.php
@@ -4,3 +4,19 @@ function isLogged()
 {
   return isset($_SESSION['id']);
 }
+
+function moveImage($image)
+{
+  $nombre = $image['name'];
+  $ruta_provisional = $image['tmp_name'];
+  $carpeta = "Fotos/";
+
+  $src = $carpeta . $nombre;
+
+  if (file_exists($src)) {
+    return $src;
+  }
+  move_uploaded_file($ruta_provisional, $src);
+  return $src;
+
+}
--- App/Views/formularioNoticia.view.php
+++ App/Views/formularioNoticia.view.php
@@ -5,10 +5,39 @@ $conection = $db->getConnection();
 $categoriasModel = new Categoria($conection);
 $categorias = $categoriasModel->getAll();
 
+$noticiasModel = new Noticia($conection);
+
 $isEditar = false;
 if (isset($parameters['data'])) {
   $isEditar = true;
   $noticia = $parameters['data']['noticia'];
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+  //Movemos la imagen y obtenemos su ruta
+  $imagenUrl = moveImage($_FILES['imagen']);
+
+  //Creamos la data que vamos a enviar
+  $data = array(
+    'titulo' => $_POST['titulo'],
+    'autor' => $_POST['autor'],
+    'fecha_hora' => $_POST['fecha'],
+    'categoria_id' => $_POST['categoria'],
+    'texto' => $_POST['cuerpo'],
+    'imagen_url' => $imagenUrl
+  );
+
+  //Actualizamos o insertamos dependiendo de si estamos en editar(isset($noticia)) o en crear
+  if (isset($noticia)) {
+
+    $noticiasModel->updateById($noticia['id'], $data);
+
+  } else {
+
+    $noticiasModel->insert($data);
+
+  }
 }
 ?>
 <?php if (!$isEditar): ?>
@@ -73,12 +102,12 @@ if (isset($parameters['data'])) {
     <label for="inpImagen" class="label-img">Agregar Imagen</label>
     <div class="imagen">
       <?php if (isset($noticia)): ?>
-      <img src="<?= "../" . $noticia["imagen_url"] ?>" alt="">
+      <img src="<?= "../../" . $noticia["imagen_url"] ?>" alt="">
       <?php endif ?>
       <!-- <img src="../Fotos\debate.png" alt=""> -->
     </div>
   </section>
 
-  <button class="btnEnviar">Enviar</button>
+  <button class="btnEnviar"><?php if(isset($noticia)){echo "Editar";}else{echo "Crear";}?></button>
 </form>
 <script src="<?= URL_PATH ?>/public/assets/javascript/formNoticia.js"></script>
