3149f13dc98ea9f6fcc186330e46b6a37dc1bf26
joaquinvesapa <joaquinvesapa@gmail.com>
2023-11-08 00:32:48 -0300

    style landing page

--- App/Views/landing.view.php
+++ App/Views/landing.view.php
@@ -1,3 +1,16 @@
+<link rel="stylesheet" href="<?= URL_PATH ?>/public/assets/css/landing.css">
 <div class="main-top">
-  <h2>Landing</h2>
+  <section class="container-landing">
+    <section class="left-side">
+      <div class="info">
+        <h2>Newspaper, tu portal de noticias</h2>
+        <a href="<?= URL_PATH ?>/noticias/pagina/1">Noticias</a>
+      </div>
+    </section>
+    <section class="right-side">
+      <div class="imagen-container" style="min-height: 200px">
+        <img style="aspect-ratio: 1/1; width: 100%;" src="<?= URL_PATH ?>/public/images/tarjetas.png" alt="">
+      </div>
+    </section>
+  </section>
 </div>
